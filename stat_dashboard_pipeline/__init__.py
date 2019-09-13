bd637e47ab6a929f3d7cb2e98a9c813c01a67396
Gregory Martin <greg@appliedsemiotics.io>
2019-09-13 15:11:59 -0400

    Add linter, lintrc

--- stat_dashboard_pipeline/__init__.py
+++ stat_dashboard_pipeline/__init__.py
@@ -2,12 +2,24 @@
 SomerStat Daily Data Dashboard
 Pipeline
 """
-name = "stat_dashboard_pipeline"
+NAME = "stat_dashboard_pipeline"
 
 class Pipeline():
+    """
+    Parent pipeline class, with methods favorable to the enduser CLI
 
+    """
     def __init__(self):
         self.placeholder_string = 'Hello Somerville'
 
     def run(self):
+        """
+        Nominal running of pipeline code
+        """
         print(self.placeholder_string)
+
+    def collect(self):
+        return
+
+    def groom(self):
+        return
